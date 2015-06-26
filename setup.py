6e13ed5f13ac811dacb11a1d370181e860ae5dd8
Jaroslaw Lachowski <jaroslaw.lachowski@wilabs.com>
2015-06-26 14:20:20 +0200

    update

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
@@ -6,32 +7,22 @@ from future import standard_library
 standard_library.install_aliases()
 from builtins import *
 from setuptools import setup, find_packages
-import subprocess
 from icybackup import __version__
 
-def get_long_desc():
-    """Use Pandoc to convert the readme to ReST for the PyPI."""
-    try:
-        return subprocess.check_output(['pandoc', '-f', 'markdown', '-t', 'rst', 'README.mdown'])
-    except:
-        print("WARNING: The long readme wasn't converted properly")
+
+dependencies = ['boto', 'python-dateutil', 'future'],
+
 
 setup(
     name='django-icybackup-jl',
     version=__version__,
     description='A Django database/media backup tool with Amazon Glacier and local folder support',
-    long_description=get_long_desc(),
+    long_description='A Django database/media backup tool with Amazon Glacier and local folder support',
     author='Jaroslaw Lachowski',
     author_email='jalachowski@gmail.com',
     url='https://github.com/jlachowski/django-icybackup',
     packages=find_packages(),
     include_package_data=True,
-    setup_requires=[
-        'setuptools_git>=0.3',
-    ],
-    install_requires=[
-        'boto',
-        'python-dateutil',
-        'future',
-    ],
+    setup_requires=['setuptools_git>=0.3', ],
+    install_requires=dependencies,
 )
